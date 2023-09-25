Nomifactory/GregTech

commit 47e0ceee8a7b9b079a19cf8f6e52da8ffa063108

Uncap Supply Exact
    You can now Supply Exact up to a Robot Arm's per-second movement rate,
    instead of the former hard cap of 64 items.

    You can now use CTRL in addition to SHIFT to multiply the step size of
    the transfer rate via the +/- buttons:
            SHIFT = 10
            CTRL  = 100
            Both  = 1000

src/main/java/gregtech/common/covers/CoverRoboticArm.java
@@ -111,7 +111,7 @@ public class CoverRoboticArm extends CoverConveyor {
     public void setTransferMode(TransferMode transferMode) {
         this.transferMode = transferMode;
         this.coverHolder.markDirty();
-        this.itemFilterContainer.setMaxStackSize(transferMode.maxStackSize);
+        this.itemFilterContainer.setMaxStackSize(this.maxItemTransferRate);
     }
 
     public TransferMode getTransferMode() {
@@ -142,7 +142,7 @@ public class CoverRoboticArm extends CoverConveyor {
     @Override
     public void readFromNBT(NBTTagCompound tagCompound) {
         super.readFromNBT(tagCompound);
-        this.transferMode = TransferMode.values()[tagCompound.getInteger("TransferMode")];
+        setTransferMode(TransferMode.values()[tagCompound.getInteger("TransferMode")]);
     }
 
 }

src/main/java/gregtech/common/covers/filter/ItemFilterContainer.java
@@ -14,6 +14,7 @@ import net.minecraftforge.items.ItemStackHandler;
 import javax.annotation.Nonnull;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.IntFunction;
 
 public class ItemFilterContainer implements INBTSerializable<NBTTagCompound> {
 
@@ -82,8 +83,23 @@ public class ItemFilterContainer implements INBTSerializable<NBTTagCompound> {
             .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.FILTER_SLOT_OVERLAY));
 
         ServerWidgetGroup stackSizeGroup = new ServerWidgetGroup(this::showGlobalTransferLimitSlider);
-        stackSizeGroup.addWidget(new ClickButtonWidget(91, 70, 20, 20, "-1", data -> adjustTransferStackSize(data.isShiftClick ? -10 : -1)));
-        stackSizeGroup.addWidget(new ClickButtonWidget(146, 70, 20, 20, "+1", data -> adjustTransferStackSize(data.isShiftClick ? +10 : +1)));
+
+        // shift -> 10x, ctrl -> 100x, shift+ctrl -> 1000x
+        final IntFunction<Consumer<Widget.ClickData>> fun = rate -> data -> {
+            int newRate = rate;
+            if(data.isCtrlClick)
+                if(data.isShiftClick)
+                    newRate = rate * 1000;
+                else
+                    newRate = rate * 100;
+            else if(data.isShiftClick)
+                newRate = rate * 10;
+
+            adjustTransferStackSize(newRate);
+        };
+
+        stackSizeGroup.addWidget(new ClickButtonWidget(91, 70, 20, 20, "-", fun.apply(-1)));
+        stackSizeGroup.addWidget(new ClickButtonWidget(146, 70, 20, 20, "+", fun.apply(1)));
         stackSizeGroup.addWidget(new ImageWidget(111, 70, 35, 20, GuiTextures.DISPLAY));
         stackSizeGroup.addWidget(new SimpleTextWidget(128, 80, "", 0xFFFFFF, () -> Integer.toString(transferStackSize)));
         widgetGroup.accept(stackSizeGroup);

src/main/java/gregtech/common/covers/TransferMode.java
@@ -4,7 +4,7 @@ import net.minecraft.util.IStringSerializable;
 
 public enum TransferMode implements IStringSerializable {
         TRANSFER_ANY("cover.robotic_arm.transfer_mode.transfer_any", Integer.MAX_VALUE),
-        TRANSFER_EXACT("cover.robotic_arm.transfer_mode.transfer_exact", 64),
+        TRANSFER_EXACT("cover.robotic_arm.transfer_mode.transfer_exact", 1024),
         KEEP_EXACT("cover.robotic_arm.transfer_mode.keep_exact", 1024);
 
         public final String localeName;

commit c3b517ca95e8d8fe94a739c9bffd6e7c3d8d207b

Robot Arm regression fix, small GUI enhancement
Fix a regression introduced in 47e0cee, where Keep Exact quantity was
limited by tier like Supply Exact. Restored prior behavior where all
tiers of Robot Arms can Keep Exact up to 1024 items.

Hide the quantity bar when the mode is switched to Transfer Any, since
the value would be ignored anyways. Quantity bar is displayed again
when the mode is switched to Keep Exact or Supply Exact.

src/main/java/gregtech/common/covers/CoverRoboticArm.java
@@ -111,7 +111,17 @@ public class CoverRoboticArm extends CoverConveyor {
     public void setTransferMode(TransferMode transferMode) {
         this.transferMode = transferMode;
         this.coverHolder.markDirty();
-        this.itemFilterContainer.setMaxStackSize(this.maxItemTransferRate);
+
+        // Workaround to prevent rate slider showing in Transfer Any
+        itemFilterContainer.forceHideRateSlider = (transferMode == TransferMode.TRANSFER_ANY);
+
+        // restrict general rate to 1024 so filters don't overflow
+        int limit = 1024;
+        // restrict Supply Exact to maximum transfer per second
+        if(transferMode == TransferMode.TRANSFER_EXACT)
+            limit = this.maxItemTransferRate;
+
+        this.itemFilterContainer.setMaxStackSize(limit);
     }
 
     public TransferMode getTransferMode() {

src/main/java/gregtech/common/covers/filter/ItemFilterContainer.java
@@ -22,6 +22,7 @@ public class ItemFilterContainer implements INBTSerializable<NBTTagCompound> {
     private final ItemFilterWrapper filterWrapper;
     private int maxStackSizeLimit = 1;
     private int transferStackSize;
+    public boolean forceHideRateSlider = false;
 
     public ItemFilterContainer(IDirtyNotifiable dirtyNotifiable) {
         this.filterWrapper = new ItemFilterWrapper(dirtyNotifiable);
@@ -130,6 +131,8 @@ public class ItemFilterContainer implements INBTSerializable<NBTTagCompound> {
     }
 
     public boolean showGlobalTransferLimitSlider() {
+        if(forceHideRateSlider)
+            return false;
         return getMaxStackSize() > 1 && filterWrapper.showGlobalTransferLimitSlider();
     }
 