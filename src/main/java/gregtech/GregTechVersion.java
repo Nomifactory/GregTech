fe6d4f943c263c3c8e598507bfc259c035c1fd0f
Exa <11907282+Exaxxion@users.noreply.github.com>
2026-06-05 18:43:17 -0700

    Restore missing fields to GregTechVersion
    Should fix mod detection between GTNE and GTCEu in InventoryBogoSort

--- src/main/java/gregtech/GregTechVersion.java
+++ src/main/java/gregtech/GregTechVersion.java
@@ -5,6 +5,10 @@ import gregtech.api.util.Version;
 public final class GregTechVersion {
 
     public static final Version VERSION = Version.parse(GTInternalTags.VERSION);
+    public static final int MAJOR    = VERSION.getNumber(0);
+    public static final int MINOR    = VERSION.getNumber(1);
+    public static final int REVISION = VERSION.getNumber(2);
+    public static final int BUILD    = VERSION.getNumber(3);
 
     private GregTechVersion() {
     }
