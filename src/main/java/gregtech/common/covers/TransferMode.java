e58478e78edecaa7976080c50d66f0d2d8be24d3
Exa <11907282+Exaxxion@users.noreply.github.com>
2026-07-03 20:58:31 -0700

    Add Supply Unique Mode for Robot Arms
    Sort of a combination of Supply and Keep Exact modes: a specific number
    of items are moved at once, but if any of that item is already in the
    destination, it will not move that item.

--- src/main/java/gregtech/common/covers/TransferMode.java
+++ src/main/java/gregtech/common/covers/TransferMode.java
@@ -5,7 +5,8 @@ import net.minecraft.util.IStringSerializable;
 public enum TransferMode implements IStringSerializable {
         TRANSFER_ANY("cover.robotic_arm.transfer_mode.transfer_any", Integer.MAX_VALUE),
         TRANSFER_EXACT("cover.robotic_arm.transfer_mode.transfer_exact", 1024),
-        KEEP_EXACT("cover.robotic_arm.transfer_mode.keep_exact", 1024);
+        KEEP_EXACT("cover.robotic_arm.transfer_mode.keep_exact", 1024),
+        TRANSFER_UNIQUE("cover.robotic_arm.transfer_mode.transfer_unique", 1024);
 
         public final String localeName;
         public final int maxStackSize;
